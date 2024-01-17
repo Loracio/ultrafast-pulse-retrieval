21b778a39e7613994a8ae334fa9153efbc0e241e
Loracio <vhloras@gmail.com>
2024-01-17 10:28:31 +0100

    Config change

--- tests/launchRandomPulseRetriever.sh
+++ tests/launchRandomPulseRetriever.sh
@@ -5,7 +5,7 @@ N=256                 # Number of samples
 deltaT=6.12           # Time spacing in femtoseconds
 centralWaveLength=790 # Central wavelength in nanometers
 TBP=2.33              # Time-Bandwidth product of the random pulse to generate
-noiseLevel=0.0        # Noise level added to the random pulse's trace
+noiseLevel=0.03        # Noise level added to the random pulse's trace
 
 # Retriever parameters
 retriever="PIE"       # Selects a switch between 'COPRA', 'GPA' or 'PIE'
